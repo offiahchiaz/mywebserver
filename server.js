1df28c7b94a30ed8f53a80adfadf5b811518a912
offiahchiaz <offiahchiaz@gmail.com>
2017-10-09 13:15:43 +0100

    Add project page

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMsg: 'Portfolio page here'
+    });
+})
+
 app.get('/bad', (req, res) => {
 
     res.send({
